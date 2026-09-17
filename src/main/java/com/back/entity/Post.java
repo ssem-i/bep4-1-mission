e8dcc2459ea94cbbcedf53484fc2445ac21948dd
ssem-i <ssem2i@naver.com>
2026-09-17 14:14:54 +0900

    commit 0004

--- src/main/java/com/back/entity/Post.java
+++ src/main/java/com/back/entity/Post.java
@@ -34,7 +34,7 @@ public class Post extends BaseIdAndTime {
         PostComment postComment = new PostComment(this, author, content);
 
         comments.add(postComment);
-
+        author.increaseActivityScore(1);
         return postComment;
     }
 
--- src/main/java/com/back/service/PostService.java
+++ src/main/java/com/back/service/PostService.java
@@ -18,7 +18,7 @@ public class PostService {
 
     public Post write(Member author, String title, String content) {
         Post post = new Post(author, title, content);
-
+        author.increaseActivityScore(3);
         return postRepository.save(post);
     }
     public Optional<Post> findById(int id) {
