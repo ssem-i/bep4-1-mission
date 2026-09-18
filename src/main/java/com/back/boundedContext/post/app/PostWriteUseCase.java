b12b2b1a3fb4d9c2692e3621981199426d518cf1
ssem-i <ssem2i@naver.com>
2026-09-18 09:45:39 +0900

    11 rsData

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -4,6 +4,9 @@ import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
+import com.back.global.rsData.RsData;
+import com.back.shared.post.dto.PostDto;
+import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +16,15 @@ public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
 
-    public Post write(Member author, String title, String content) {
+    //public Post write(Member author, String title, String content) {
+    public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
-        return post;
+
+        eventPublisher.publish(
+                new PostCreatedEvent(
+                        new PostDto(post)
+                )
+        );
+        return new RsData<>("200-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
     }
 }
