9ccc2ce987541cc1d31aa34dd092e75c7142affd
ssem-i <ssem2i@naver.com>
2026-09-18 09:52:48 +0900

    12 rsData member

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -5,7 +5,7 @@ import com.back.boundedContext.member.out.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import com.back.global.rsData.RsData;
 import java.util.Optional;
 
 @Service
@@ -20,7 +20,7 @@ public class MemberFacade {
     }
 
     @Transactional
-    public Member join(String username, String password, String nickname) {
+    public RsData<Member> join(String username, String password, String nickname) {
         return memberJoinUseCase.join(username, password, nickname);
     }
 
