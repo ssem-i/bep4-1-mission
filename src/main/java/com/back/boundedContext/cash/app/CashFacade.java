56dcf21a783a0e1545f6417b7b15844b0ae13949
ssem-i <ssem2i@naver.com>
2026-09-22 14:09:32 +0900

    37 38

--- src/main/java/com/back/boundedContext/cash/app/CashFacade.java
+++ src/main/java/com/back/boundedContext/cash/app/CashFacade.java
@@ -2,13 +2,13 @@ package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.domain.Wallet;
+import com.back.shared.market.dto.OrderDto;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 import com.back.shared.cash.dto.CashMemberDto;
-import com.back.shared.market.event.MarketOrderPaymentRequestedEvent;
 
 @Service
 @RequiredArgsConstructor
@@ -39,8 +39,8 @@ public class CashFacade {
     }
 
     @Transactional
-    public void handle(MarketOrderPaymentRequestedEvent event) {
-        cashCompleteOrderPaymentUseCase.handle(event);
+    public void completeOrderPayment(OrderDto order, long pgPaymentAmount) {
+        cashCompleteOrderPaymentUseCase.completeOrderPayment(order, pgPaymentAmount);
     }
 
     @Transactional(readOnly = true)
--- src/main/java/com/back/boundedContext/member/domain/MemberPolicy.java
+++ src/main/java/com/back/boundedContext/member/domain/MemberPolicy.java
@@ -1,13 +1,18 @@
 package com.back.boundedContext.member.domain;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.Value;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Service
 public class MemberPolicy {
-    private static final int PASSWORD_CHANGE_DAYS = 90;
+    private static int PASSWORD_CHANGE_DAYS;
+
+    @Value("${custom.member.password.changeDays}")
+    public void setPasswordChangeDays(int days) {
+        PASSWORD_CHANGE_DAYS = days;
+    }
 
     public Duration getNeedToChangePasswordPeriod() {
         return Duration.ofDays(PASSWORD_CHANGE_DAYS);
