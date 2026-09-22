7655dab384c36084eaf5b0db2684dc23c4afec23
ssem-i <ssem2i@naver.com>
2026-09-22 16:17:18 +0900

    42

--- src/main/java/com/back/boundedContext/payout/app/PayoutCreatePayoutUseCase.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutCreatePayoutUseCase.java
@@ -1,13 +1,29 @@
 package com.back.boundedContext.payout.app;
 
 import com.back.shared.payout.dto.PayoutMemberDto;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.back.boundedContext.payout.domain.Payout;
+import com.back.boundedContext.payout.domain.PayoutMember;
+import com.back.boundedContext.payout.out.PayoutMemberRepository;
+import com.back.boundedContext.payout.out.PayoutRepository;
 
-@Slf4j
+@RequiredArgsConstructor
 @Service
 public class PayoutCreatePayoutUseCase {
-    public void createPayout(PayoutMemberDto payee) {
-        log.debug("createPayout.payee: {}", payee.getId());
+
+    private final PayoutRepository payoutRepository;
+    private final PayoutMemberRepository payoutMemberRepository;
+
+    public Payout createPayout(PayoutMemberDto payee) {
+        PayoutMember _payee = payoutMemberRepository.getReferenceById(payee.getId());
+
+        Payout payout = payoutRepository.save(
+                new Payout(
+                        _payee
+                )
+        );
+
+        return payout;
     }
 }
